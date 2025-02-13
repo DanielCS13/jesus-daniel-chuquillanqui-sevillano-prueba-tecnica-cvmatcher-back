4403de0c98e4870ba790e8dd7018e5bf2906b827
Jesús Daniel Chuquillanqui Sevillano <t022700620@unitru.edu.pe>
2025-02-13 01:02:33 -0500

    checking

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScrappingModule } from './scrapping/scrapping.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ScrappingModule, ConfigModule.forRoot(), SupabaseModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
